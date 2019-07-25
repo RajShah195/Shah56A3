31f08de6f0e352707d706efa5f852cb0c72a105b
RajShah195 <52724653+RajShah195@users.noreply.github.com>
2019-07-24 22:19:41 -0400

    Delete myBooks.ts

--- assets/data/myBooks.ts
+++
@@ -1,7 +0,0 @@
-import {BooksInfo} from '../../app/BooksInfo';
-export const MYBOOKS: BooksInfo[] = [
-    {BookName: 'Subtle Art of not giving a fuck', BookAuthor: 'Mark Mansion', Genere:'Self-help book', Date: '09-13-2016',Picture: 'img1.png'},
-    {BookName: 'Harry Potter and Chamber of secrets', BookAuthor: 'J.K Rowling', Genere :'Fantasy', Date: '07-02-1998',Picture: 'img2.png'},
-    {BookName: 'The Alchemist', BookAuthor: 'Paulo Coelho', Genere :'Quest,Fantasy fiction', Date: '1998',Picture: 'img3.png'},
-    {BookName: 'Lord of the Rings', BookAuthor: 'J.R.R', Genere :'Fantasy fiction', Date: '12-19-2001',Picture: 'img4.png'}
-]
